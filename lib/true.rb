e3da25c80e89aaff062bff1ee5b34f6591c4e197
wǒ_is神仙 <i@mrzhang.me>
2014-04-29 20:58:39 +0800

    fix the name of SASS_PATH

--- lib/true.rb
+++ lib/true.rb
@@ -4,9 +4,9 @@ begin
   Compass::Frameworks.register('true', :stylesheets_directory => true_stylesheets_path)
 rescue LoadError
   # compass not found, register on the Sass path via the environment.
-  if ENV.has_key?("SASSPATH")
-    ENV["SASSPATH"] = ENV["SASSPATH"] + File::PATH_SEPARATOR + true_stylesheets_path
+  if ENV.key? 'SASS_PATH'
+    ENV['SASS_PATH'] += File::PATH_SEPARATOR + true_stylesheets_path
   else
-    ENV["SASSPATH"] = true_stylesheets_path
+    ENV['SASS_PATH'] = true_stylesheets_path
   end
 end
