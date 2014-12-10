3b8fdcad1b16160f4e04c5246dd4cd112590e941
Carl Meyer <carl@oddbird.net>
2014-12-09 23:31:57 -0700

    Can parse a non-output passing test result.

--- test/test_main.js
+++ test/test_main.js
@@ -1,17 +1,94 @@
+var CODE = process.env.COVER ? '../lib-cov/' : '../lib/';
+
 var expect = require('chai').expect;
-var parse = require('../lib/main.js').parse;
+var main = require(CODE + 'main.js');
 
 describe('#parse', function () {
-  it('parses a non-output test', function () {
+  it('parses a passing non-output test', function () {
     var css = [
       '[data-module="M"] [data-test="T"] .assert-equal {',
       '  -result: PASS;',
       "  -description: 'Desc';",
       '}'
     ].join('\n');
-    var expected = [
-    ];
+    var expected = [{
+      module: "M",
+      tests: [{
+        test: "T",
+        assertions: [{
+          description: "Desc",
+          assert: "equal",
+          passed: true,
+        }],
+      }],
+    }];
 
-    expect(parse(css)).to.deep.equal(expected);
+    expect(main.parse(css)).to.deep.equal(expected);
+  });
+
+  it('throws on more than one selector', function () {
+    var attempt = function () { main.parse('.foo, .bar {}'); };
+
+    expect(attempt).to.throw(/too many selectors: .foo, .bar/);
+  });
+});
+
+
+describe('#parseSelector', function () {
+  it('parses non-output test selector', function () {
+    var sel = '[data-module="M"] [data-test="T"] .assert-equal';
+    var exp = {
+      output: false,
+      module: "M",
+      test: "T",
+      assert: "equal",
+    };
+
+    expect(main.parseSelector(sel)).to.deep.equal(exp);
+  });
+
+  it('parses output test selector', function () {
+    var sel = '[data-module="M"] [data-test="T"] [data-assert="A"] .input';
+    var exp = {
+      output: true,
+      module: "M",
+      test: "T",
+      assert: "A",
+      type: 'input',
+    };
+
+    expect(main.parseSelector(sel)).to.deep.equal(exp);
+  });
+
+  it('throws an error on badly-quoted attr selector', function () {
+    var attempt = function () {
+      main.parseSelector('[data-module=A]');
+    };
+
+    expect(attempt).to.throw(/badly-quoted attribute selector value: A/);
+  });
+
+  it('throws an error on unknown attr selector', function () {
+    var attempt = function () {
+      main.parseSelector('[data-foo="bar"]');
+    };
+
+    expect(attempt).to.throw(/unexpected attribute selector: data-foo/);
+  });
+
+  it('throws an error on unknown class selector', function () {
+    var attempt = function () {
+      main.parseSelector('.foo');
+    };
+
+    expect(attempt).to.throw(/unexpected selector component: .foo/);
+  });
+
+  it('throws an error on unknown selector type', function () {
+    var attempt = function () {
+      main.parseSelector('#foo');
+    };
+
+    expect(attempt).to.throw(/unexpected selector component: #foo/);
   });
 });
