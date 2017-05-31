f1c364f66c7624de698aa187bde45206e75f3fd7
Miriam Suzanne <miriam@oddbird.net>
2017-05-31 17:53:20 -0600

    No more ruby

--- Makefile
+++
@@ -1,7 +0,0 @@
-BIN = ./node_modules/.bin
-
-
-.PHONY: test
-test:
-	npm test
-	rake test
--- lib/true.rb
+++
@@ -1,9 +0,0 @@
-true_stylesheets_path = File.expand_path(File.join(File.dirname(__FILE__), '..', 'sass'))
-ENV['SASS_PATH'] ||= ''
-begin
-  require 'compass'
-  ENV['SASS_PATH'] = ENV['SASS_PATH'] + File::PATH_SEPARATOR + Compass.configuration.sass_load_paths.join(File::PATH_SEPARATOR)
-rescue LoadError
-end
-# compass not found, register on the Sass path via the environment.
-ENV['SASS_PATH'] += File::PATH_SEPARATOR + true_stylesheets_path
