e6bbf5efc9ee9c65937b32a1d76f91b855074f6a
Jonny Gerig Meyer <jonny@oddbird.net>
2025-12-03 16:56:02 -0500

    remove comment

--- vitest.config.js
+++ vitest.config.js
@@ -4,7 +4,7 @@ const { defineConfig } = require('vitest/config');
 
 module.exports = defineConfig({
   test: {
-    // Make describe, it, expect, etc. available globally (like Jest)
+    // Make describe, it, expect, etc. available globally
     globals: true,
 
     forceRerunTriggers: ['**/*.scss'],
@@ -16,14 +16,7 @@ module.exports = defineConfig({
     coverage: {
       enabled: true,
       reportOnFailure: true,
-
-      // An array of glob patterns indicating a set of files for which coverage information should be collected
-      // include: ['src/**/*.{js,ts}'],
-
-      // A list of reporter names that Vitest uses when writing coverage reports
       reporter: ['text-summary', 'html'],
-
-      // An object that configures minimum threshold enforcement for coverage results
       thresholds: {
         branches: 95,
         functions: 95,
